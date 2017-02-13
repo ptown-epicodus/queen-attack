8f6b58edd3a52890fb9fcbda3a0bbf8dbc181320
Leah Sherrell and Patrick McGreevy <leah@>
2017-02-13 14:44:57 -0800

    Implement basic interface

--- app/app.php
+++ app/app.php
@@ -2,6 +2,7 @@
     date_default_timezone_set('America/Los_Angeles');
 
     require_once __DIR__ . '/../vendor/autoload.php';
+    require_once __DIR__ . '/../src/Queen.php';
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -10,6 +11,16 @@
 
     $app->get('/', function() use ($app) {
         return $app['twig']->render('queen_attack.html.twig');
+    });
+
+    $app->post('/queen_attack', function() use ($app) {
+        $queen = new Queen($_POST['start-x'], $_POST['start-y']);
+        $end = array($_POST['end-x'], $_POST['end-y']);
+
+        return $app['twig']->render('queen_attack_result.html.twig', array(
+            'start' => $queen,
+            'end' => $end
+        ));
     });
 
     return $app;
